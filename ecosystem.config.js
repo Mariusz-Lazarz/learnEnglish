2f66a6148b22a31d1966bef31d2f295b77b0ee7a
Mariusz-Lazarz <mariusz.lazarz97@gmail.com>
2026-05-31 00:14:21 +0200

    feat: test ci cd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  apps: [{
-    name: 'learn-english',
-    script: 'node_modules/.bin/next',
-    args: 'start',
-    instances: 1,
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 3000
-    }
-  }]
-}
+  apps: [
+    {
+      name: "learn-english",
+      script: "node_modules/.bin/next",
+      args: "start",
+      instances: 1,
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3001,
+      },
+    },
+  ],
+};
