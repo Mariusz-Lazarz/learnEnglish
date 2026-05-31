96908f9f042f02880357fda6798436b0067afeb4
Mariusz-Lazarz <mariusz.lazarz97@gmail.com>
2026-05-31 12:23:38 +0200

    feat: test cicd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "learn-english",
-      script: "/root/.nvm/versions/node/v20.20.2/bin/node",
-      args: "/root/learnEnglish/node_modules/.bin/next start -p 3001",
+      script: "./node_modules/.bin/next",
+      args: "start -p 3001",
       cwd: "/root/learnEnglish",
       instances: 1,
       exec_mode: "fork",
